d259b42b780e34f3792f34f1a0d992eba8216a8f
S.D.Norberhuis <steffan@norberhuis.nl>
2013-06-07 13:14:03 +0200

    TorrentfinderControl now correctly calls back.

--- Tribler/Test/TUPT/TorrentFinder/test_TorrentFinderControl.py
+++ Tribler/Test/TUPT/TorrentFinder/test_TorrentFinderControl.py
@@ -6,16 +6,49 @@ from Tribler.Test.TUPT.TorrentFinder.TorrentFinderStubs import TorrentDefStub
 from Tribler.TUPT.TorrentFinder.TorrentFinderControl import TorrentFinderControl
 from Tribler.TUPT.Movie import Movie
 
+import time
+
 class TestTorrentFinderControl(unittest.TestCase):
     '''Test class to test TorrentFinderControl'''
     
-    def setUp(self):
+    def setUp(self, ):
+        """Setup before testing"""
         #Arrange
         self.__movie = Movie()
         self.__movie.dictionary['title'] = 'TestMovie'
-        self.__torrentFinderControl = TorrentFinderControl(TorrentFinderPluginManagerStub(), self.__movie)       
+        self.__pluginmanager = TorrentFinderPluginManagerStub()
+        self.__torrentFinderControl = TorrentFinderControl(self.__pluginmanager, self.__movie, self.callback)
+        self.__timesCallbacked = 0       
+    
+    def test_HasHDTorrent_HasNoTorrent(self):
+        #Act
+        result = self.__torrentFinderControl.HasHDTorrent()
+        #Assert
+        self.assertFalse(result)
+        
+    def test_HasHDTorrent_HasTorrent(self):
+        #Arrange
+        self.__torrentFinderControl.FindTorrents()
+        #Act
+        result = self.__torrentFinderControl.HasHDTorrent()
+        #Assert
+        self.assertTrue(result)
+        
+    def test_HasSDTorrent_HasNoTorrent(self):
+        #Act
+        result = self.__torrentFinderControl.HasSDTorrent()
+        #Assert
+        self.assertFalse(result)
+        
+    def test_HasSDTorrent_HasTorrent(self):
+        #Arrange
+        self.__torrentFinderControl.FindTorrents()
+        #Act
+        result = self.__torrentFinderControl.HasSDTorrent()
+        #Assert
+        self.assertTrue(result)
      
-    def test_FindTorrent_GetResults(self):      
+    def test_FindTorrent_GetResults(self):
         #Act
         self.__torrentFinderControl.FindTorrents()
         #Assert
@@ -23,7 +56,7 @@ class TestTorrentFinderControl(unittest.TestCase):
         self.assertTrue(len(self.__torrentFinderControl.GetSDTorrentList()) > 0)
         
     def test_ProcessTorrentDef_addHDDefinition(self):
-        #Arrange
+         """Test processing a HD TorrentDef."""
          torrentDef =  TorrentDefStub(True, self.__movie)
          #Act
          self.__torrentFinderControl.ProcessTorrentDef(torrentDef, 0.5)
@@ -32,13 +65,41 @@ class TestTorrentFinderControl(unittest.TestCase):
          self.assertEqual(0, len(self.__torrentFinderControl.GetSDTorrentList()))
          
     def test_ProcessTorrentDef_addSDDefinition(self):
-        #Arrange
+         """Test processing a SD TorrentDef."""
          torrentDef =  TorrentDefStub(False, self.__movie)
          #Act
          self.__torrentFinderControl.ProcessTorrentDef(torrentDef, 0.5)
          #Assert
          self.assertTrue(len(self.__torrentFinderControl.GetSDTorrentList()) > 0)
          self.assertEqual(0, len(self.__torrentFinderControl.GetHDTorrentList()))
+    
+    def test_AsynchronosCallback_CallbackCalled(self):
+        """Test if the callback is called on results"""
+        self.__torrentFinderControl.start()
+        #Assert
+        self.__torrentFinderControl.join()
+        self.assertTrue(self.__timesCallbacked)
+
+    def test_FindTorrent_CallBack_OnlyCalledOnChange_NoChange(self):
+        """Test if the callback is only called on a change in results."""
+        #Act
+        self.__torrentFinderControl.start()
+        self.__torrentFinderControl.join()
+        #Assert
+        self.assertTrue(1, self.__timesCallbacked)
+        
+    def test_FindTorrent_CallBack_OnlyCalledOnChange_Change(self):
+        """Test if the callback is only called on a change in results."""
+        #Arrange
+        self.__torrentFinderControl = TorrentFinderControl(TorrentFinderPluginManagerStub(changedResult = True), self.__movie, self.callback)
+        #Act
+        self.__torrentFinderControl.start()
+        self.__torrentFinderControl.join()
+        #Assert
+        self.assertEqual(2, self.__timesCallbacked)
+    
+    def callback(self):
+        self.__timesCallbacked += 2  
     
 if __name__ == '__main__':
     unittest.main()
